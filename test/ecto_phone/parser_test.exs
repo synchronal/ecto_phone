019e66de4cca90bbcb75be7e41504b35e0e9ca70
Eric Saxby <sax@livinginthepast.org>
2025-07-02 07:29:49 -0700

    Simplify names of parser tests

--- test/ecto_phone/parser_test.exs
+++ test/ecto_phone/parser_test.exs
@@ -5,11 +5,11 @@ defmodule EctoPhone.ParserTest do
   alias EctoPhone.Parser
 
   describe "parse/2" do
-    test "works with Swedish landline" do
+    test "parses Swedish landlines" do
       assert {:ok, _} = Parser.parse("+4684450440", default_prefix: "46", format: :international)
     end
 
-    test "works with Swedish landline without prefix" do
+    test "parses Swedish landlines without prefix" do
       assert {:ok, _} = Parser.parse("020 899 123", default_prefix: "46", format: :international)
     end
   end
